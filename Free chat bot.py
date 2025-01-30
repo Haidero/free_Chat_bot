9c60cb4fc72c34ee34c03d616d966da5dda212b4
zeeshan_haider <haiderjaan713@gmail.com>
2025-01-30 19:46:33 +0500

    hi

--- Free chat bot.py
+++ Free chat bot.py
@@ -1,1 +1,15 @@
-print("Hi, Wellcome to free_chat_bot")
+print("Hi, Wellcome to free_chat_bot, how can i help you?")
+while True:
+    user_input = input("You: ").lower()  # Convert input to lowercase
+
+    if user_input == "hello":
+        print("Free_chatbot: Hello! How are you?")
+    elif user_input == "how are you?":
+        print("Free_chatbot: I'm a Free Chatbot program, but I’m doing well. Thank you!")
+    elif user_input == "bye":
+        print("Free_chatbot: Goodbye! Have a great day!")
+        break  # Exit loop when user says "bye"
+    elif user_input == "i am fine":
+        print("Free_chatbot: That's great!")
+    else:
+        print("Free_chatbot: Sorry, I didn't understand that.")
